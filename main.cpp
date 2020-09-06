b82d55b156a0d4cf725b460800959ba88e659577
shreyas112 <69191843+shreyas112@users.noreply.github.com>
2020-09-06 20:39:03 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  shreyas mycaptain code
+//
+//  Created by sasikumaran nair p on 06/09/20.
+//  Copyright © 2020 sasikumaran nair p. All rights reserved.
+//
+
+#include <iostream>
+int main(void)
+{
+    double a,b;
+    std::cout<<"Enter two number of your choice to be multiplied:\n";
+    std::cin>>a;
+    std::cin>>b;
+    double sum;
+    sum=a*b;
+    std::cout<<"Your product is:"<<sum;
+    std::cout<<"\n";
+}
+
+//finding sizeof
+//    std::cout<<"Size of char:"<<sizeof(char);
+//    std::cout<<"\n";
+//    std::cout<<"Size of int:"<<sizeof(int);
+//    std::cout<<"\n";
+//    std::cout<<"Size of float:"<<sizeof(float);
+//    std::cout<<"\n";
+//    std::cout<<"Size of double:"<<sizeof(double);
+//    std::cout<<"\n";
+
+
+//first code
+//    std::cout<<"Hello World";
+
+//entering input
+//    int age;
+//    std::cout<<"Enter your age:\n";
+//    std::cin>>age;
+//    std::cout<<"Your age is:"<<age;
+//
